145a8f6be0db770b84052082dbb696dd666d79c5
ela <190925955+elan-thinks@users.noreply.github.com>
2025-06-26 19:13:52 +0300

    initia code

--- 01_basics/firstCode.py
+++ 01_basics/firstCode.py
@@ -9,12 +9,12 @@ def main():
     print("🧮 Welcome to Simple Calculator!")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    
+
     print("Choose an operation: ")
     print("1. Add")
     print("2. Substract")
     choice = input("Enter 1 or 2 : ")
-    
+
     if choice == "1":
         result = add(num1,num2)
         print(f"The result is {result}")
@@ -23,8 +23,7 @@ def main():
         print(f"The result is {result}")
     else:
         print("❌ Invalid choice.")
-        
+
 if __name__ == "__main__":
     main()
-        
-        
+
